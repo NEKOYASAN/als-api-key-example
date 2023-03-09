9ce0ea07de01a646f6e679eaaf0bfd4260436b3c
NEKOYASAN <hello@nekoyasan.me>
2023-03-10 01:25:07 +0900

    add(env): MAP Environments

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,11 @@
 /// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+  readonly VITE_MAP_REGION: string;
+  readonly VITE_MAP_NAME: string;
+  readonly VITE_MAP_APP_KEY: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
